16fb3690c488d82536559b4d379ec7bfc55c18f9
vova07 <bazillio07@yandex.ru>
2014-06-05 12:32:41 +0300

    PhpDoc fix.

--- models/Upload.php
+++ models/Upload.php
@@ -8,12 +8,12 @@ use yii\base\Model;
  * Class Upload
  * @package vova07\imperavi\models
  *
- * @property yii\web\UploadedFile|null $file Uploaded file
+ * @property \yii\web\UploadedFile|null $file Uploaded file
  */
 class Upload extends Model
 {
     /**
-     * @var yii\web\UploadedFile|null $file Uploaded file
+     * @var \yii\web\UploadedFile|null $file Uploaded file
      */
     public $file;
 }
