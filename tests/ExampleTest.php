2fd1ff20d7f5f163d19f4ce92c0c7bfafdddc906
vova07 <bazillio07@yandex.ru>
2015-03-01 17:08:14 +0200

    Implement new skeleton #28.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace vova07\imperavi\test;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
